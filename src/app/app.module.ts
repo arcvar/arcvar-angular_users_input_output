44719cd867b2a7def2268078bc31ca524e942699
arcvar <arvar@nassibi.com>
2024-04-02 23:31:02 +0000

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { UsersComponent } from './users/users.component';
+import { UserSingleComponent } from './users/user-single/user-single.component';
+
+@NgModule({
+  declarations: [AppComponent, UsersComponent, UserSingleComponent],
+
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/users/user-single/user-single.component.ts
@@ -0,0 +1,12 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-user-single',
+  templateUrl: './user-single.component.html',
+  styleUrls: ['./user-single.component.css'],
+})
+export class UserSingleComponent {
+  @Input('aliasSingleUser') singleUser: string;
+  @Input() userIndex: number;
+  @Output() deleteUser: EventEmitter<number> = new EventEmitter<number>();
+}
